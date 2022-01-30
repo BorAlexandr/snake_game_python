5bf5e683ea717a8a9ca91145afd51fa326e83d51
BorAlexandr <90643471+BorAlexandr@users.noreply.github.com>
2022-01-30 10:55:40 +0300

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -6,16 +6,32 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.current_score = 0
-        self.color("white")
-        self.hideturtle()
-        self.penup()
-        self.goto(0, 270)
+        self.inscriptions = {}
+        self.add_inscription()
+
+        with open("high_score.txt", "r") as f:
+            self.high_score = f.read()
+
+    def add_inscription(self):
+        self.inscriptions["current_score"] = Turtle()
+        self.inscriptions["high_score"] = Turtle()
+        for i in self.inscriptions:
+            self.inscriptions[i].color("white")
+            self.inscriptions[i].hideturtle()
+            self.inscriptions[i].penup()
+        self.inscriptions["current_score"].goto(-100, 270)
+        self.inscriptions["high_score"].goto(100, 270)
 
     def show_score(self):
-        self.clear()
-        out_score = f"Score is {self.current_score}"
-        self.write(out_score, move=False, align="center", font=("Arial", 14, "bold"))
+        self.inscriptions["current_score"].clear()
+        self.inscriptions["high_score"].clear()
+        self.inscriptions["current_score"].write(f"Score: {self.current_score}",
+                                                 move=False, align="center",
+                                                 font=("Arial", 14, "bold"))
+        self.inscriptions["high_score"].write(f"High score: {self.high_score}",
+                                              move=False, align="center",
+                                              font=("Arial", 14, "bold"))
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", move=False, align="center", font=("Arial", 14, "bold"))
+    def record_hight_score(self, score):
+        with open("high_score.txt", "w") as high_score:
+            high_score.write(str(score))
